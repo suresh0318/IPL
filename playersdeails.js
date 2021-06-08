5f6803a8288d7f15e797aa6f606e2500b1bf5fed
suresh0318 <40860944+suresh0318@users.noreply.github.com>
2021-06-08 20:04:53 +0530

    Update playersdeails.js

--- playersdeails.js
+++ playersdeails.js
@@ -1,9 +1,1 @@
-var playersInfo = JSON.parse(localStorage.getItem("players-data"));
 
-var teamId = window.location.search.split("")[1];
-var playerId = window.location.search.split("=")[1];
-
-
-
-console.log(teamId)
-console.log(playerId)
